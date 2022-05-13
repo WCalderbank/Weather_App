f0bec6c5c1a3cc00eac1a1fede315a261051e6da
WCalderbank <wcalderbank99@gmail.com>
2022-05-13 11:59:54 +0100

    api key obtained

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -1,8 +1,35 @@
 import "./App.css";
 import React from "react";
+const weatherApi = {
+  key: "f275f7ed536cb14807cf3876a63270d1",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
-  return <div className="App"></div>;
+  const dateCreator = (d) => {
+    let days = [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ];
+  };
+  return (
+    <div className="App">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Location..." />
+        </div>
+        <div className="location-box">
+          <div className="location">Manchester, UK</div>
+          <div className="date">{dateCreator(new Date())}</div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
